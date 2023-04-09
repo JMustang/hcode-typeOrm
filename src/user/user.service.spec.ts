a9704b13bd50c0e3b4fdd4843dd9fb0c289497a8
Junior <eecfredes@gmail.com>
2023-04-09 11:35:36 -0300

    :rocket: userService tests

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,6 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { userRepositoryMock } from '../testing/user-repository.mock';
+import { CreateUserEntity } from './entities/user.entity';
+import { Role } from '../enums/role.enum';
+import { UserEntity } from './entity/user.entity';
+
+const UserEntityList: UserEntity[] = [{}];
 
 describe('UserService', () => {
   let userService: UserService;
@@ -15,4 +20,20 @@ describe('UserService', () => {
   test('validate the definition', () => {
     expect(userService).toBeDefined();
   });
+
+  describe('Create', () => {
+    test('method create', async () => {
+      const data: CreateUserEntity = {
+        birthAt: '2000-01-01',
+        email: 'user@email.com',
+        name: 'user',
+        password: '123455',
+        role: Role.User,
+      };
+      const result = await userService.create(data);
+    });
+  });
+  describe('read', () => {});
+  describe('update', () => {});
+  describe('delete', () => {});
 });
